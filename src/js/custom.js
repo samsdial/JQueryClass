bec8a75a9d840f7ac6ee0e94b095303bbcbd99d5
smartinez <smartinez@optimeconsulting.com>
2018-12-07 11:08:43 -0500

    Add  // Eventos, on Click, on leave, on over, And others

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,76 +1,22 @@
-$(document).ready(function($){
-    $('#submit').click(function(){
-        var name        = $("#name").val();
-        var lastName    = $("#lastName").val();
-        var email       = $("#email").val();
-        var validacion_email = /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/;
-        var phone       = $("#phone").val();
-        var city       = $("#city").val();
+$(".btn-js").on("click", function () {
+    console.log("click");
+});
+$(".img-js").on("mouseenter", function () {
+    //console.log("enter")
+    $(this).addClass("img-thumbnail");
+});
+$(".img-js").on("mouseleave", function () {
+    //console.log("leave")
+    $(this).removeClass("img-thumbnail");
+})
+// Controles especiales para linjs
+$(".btn-js").on("click", function (e) {
+    e.preventDefault();
+    var link = $(this).attr("href");
+    console.log(link + ' Info Link')
+});
 
-        if(name == "" || lastName == "" || email == "" || phone == "" || city == ""){
-            if(name == ""){
-                $(".name + .error").addClass("i");
-                $("#name").focus();
-                return false;
-            }else{
-                $(".name + .error").removeClass("i");
-            }
-            if(lastName == ""){
-                $(".lastname + .error").addClass("i");
-                $("#lastName").focus();
-                return false;
-            }else{
-                $(".lastname + .error").removeClass("i");
-            }
-            if(email == "" || !validacion_email.test(email)){
-                $(".email + .error").addClass("i");
-                $("#email").focus();
-                return false;
-            }else{
-                $(".email + .error").removeClass("i");
-            }
-            if(phone == ""){
-                $(".cel + .error").addClass("i");
-                $("#phone").focus();
-                return false;
-            }else{
-                $(".cel + .error").removeClass("i");
-            }
-            if(city == ""){
-                $(".city + .error").addClass("i");
-                $("#city").focus();
-                return false;
-            }else{
-                $(".city + .error").removeClass("i");
-            }
-        }else{
-            $(".city + .error").removeClass("i");
-            var datos = '&name=' + name +
-                '&lastName=' + lastName +
-                '&email=' + email +
-                '&phone=' + phone +
-                '&city=' + city;
-            $.ajax({
-                method:"Post",
-                url:"contact.php",
-                dataType:"json",
-                data:datos,
-            }).done(function( msg ){
-                if(msg.success){
-                    $('.alerta p').fadeIn("slow");
-                    $('.alerta p').html('Muchas Gracias por contáctarnos, pronto nos estaremos comunicando contigo.');
-                    $('#name').val("");
-                    $('#lastNamee').val("");
-                    $('#email').val("");
-                    $('#phone').val("");
-                    $('#city').val("");
-                }else{
-                    $('.alerta p').fadeIn("slow");
-                    $('.alerta p').html('Error, intente más tarde por favor.');
-                }
-            });
-            return false;
-        }
-    });
+$(document).ready(function(){
+    console.log("PG lista");
 });
 
