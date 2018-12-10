da3d572ff05c40951b7dcc851f8b088e623d8773
smartinez <smartinez@optimeconsulting.com>
2018-12-10 10:08:23 -0500

    Add  // OBJETOS Insertando input por JQ

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,36 +1,14 @@
-// Funcion anomima
-var carro = {
-    encedido: false,
-    llantas:4,
-    puertas:5,
-    fabricante:{
-        creador:"toyora",
-        telefono: "555-5555",
-        direction: "xyz Avenida 123"
-    },
-    colores:["azul", "Blanco", "Rojo", "Negro"],
-    arrancar: function () {
-        if(carro.encedido){
-            console.log("El veiculo estaba encedido");
-        }else {
-            console.log("Encendiendo el motor");
-            this.encedido = true;
-        }
-    },
-    apagar: function () {
-        if( carro.encedido === false){
-            console.log("El veiculo ya estaba Apagado");
-        }else {
-            console.log("Apagando el motor");
-            this.encedido = false;
-        }
-    }
-};
-//carro.arrancar();
-//carro.apagar();
-$(".js_encender").on("click", function () {
-    carro.arrancar();
-});
-$(".js_apagar").on("click", function () {
-    carro.apagar();
-});
+//
+$("<input>", {
+    type:           "text",
+    class:          "form-control",
+    placeholder:    "Ingrese su nombre"
+})
+    .appendTo(".acction")
+    .on("keypress", function ( e ) {
+        console.log( e.keyCode )
+    });
+    $("input").css({
+       position: "relative",
+       top: "100px",
+    });
