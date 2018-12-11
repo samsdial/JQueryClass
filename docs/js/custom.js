2a90d977e7e4016f94628299ba1921cce23e67fa
smartinez <smartinez@optimeconsulting.com>
2018-12-11 10:06:46 -0500

    Add  // Animaciones Simples con animate de JQ

--- docs/js/custom.js
+++ docs/js/custom.js
@@ -1,2 +1,2 @@
-"use strict";!function(){var i=$(".cajaRoja");$("#btnSize").on("click",function(){i.animate({width:"+=100px",height:"+=100px"},1e3,function(){})})}();
+"use strict";!function(){var i=$(".cajaRoja");$("#btnSize").on("click",function(){i.animate({width:"+=100px",height:"+=100px"},function(){console.log("termino An")}).animate({width:"+=100px",height:"+=100px"}).animate({opacity:.1},1500,function(){$(this).remove()})})}();
 //# sourceMappingURL=custom.js.map
--- src/js/custom.js
+++ src/js/custom.js
@@ -5,8 +5,15 @@
       $cajaRoja.animate({
          width: '+=100px',
          height: '+=100px',
-      },1000 ,function () {
-
+      },function () {
+         console.log("termino An");
+      }).animate({
+         width: '+=100px',
+         height: '+=100px',
+      }).animate({
+         opacity: 0.1
+      },1500, function () {
+         $(this).remove();
       });
    })
 })();
