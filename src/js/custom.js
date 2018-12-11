dcfcf5e7f8e0f3fadf693dee5837cd2a98ab56ea
smartinez <smartinez@optimeconsulting.com>
2018-12-11 16:00:05 -0500

    Add  // Animaciones Avanzadas TweenMax

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,35 +1,51 @@
 //
 (function () {
-   var $cajaRoja = $(".cajaRoja");
+
    function mover( dir ) {
-      $cajaRoja.clearQueue();
+        var $cajaRoja = $(".cajaRoja");
+        $cajaRoja.clearQueue();
+        var $cajaAzul = $(".cajaAzul");
+        var tl = new TimelineMax();
+
       switch ( dir ) {
          case "top":
             $cajaRoja.animate({
-               top: "-=50px"
-            },200);
+               top: "-=100px"
+            },450);
+            tl.to( $cajaAzul, 0.45, { y:"-=100"} );
          break;
          case "down":
             $cajaRoja.animate({
-               top: "+=50px"
-            }, 200);
+               top: "+=100px"
+            }, 450);
+             tl.to( $cajaAzul, 0.45, { y:"+=100"} );
          break;
          case "izq":
             $cajaRoja.animate({
-               left: "-=50px"
-            }, 200);
+               left: "-=100px"
+            }, 450);
+             tl.to( $cajaAzul, 0.45, { x:"-=100"} );
          break;
          case "der":
             $cajaRoja.animate({
-               left: "+=50px"
+               left: "+=100px"
             }, 200);
+             tl.to( $cajaAzul, 0.45, { x:"+=100"} );
          break;
-         default:
+          case "reset":
             $cajaRoja.animate({
                top: "0",
-               left: "0"
-            },1000);
-
+               left: "0",
+                width: "50px",
+                height: "50px",
+            },450);
+             tl.to( $cajaAzul, 0.45, {
+                 y:"0",
+                 x:"0",
+                 width:"50px",
+                 height:"50px",
+                 backgroundColor: "blue",
+             } );
 
       }
    }
@@ -51,7 +67,24 @@
          default:
             mover("reset")
       }
-   })
+   });
+   $("#js-ancho").on("click", function () {
+       var $cajaRoja = $(".cajaRoja");
+       $cajaRoja.clearQueue();
+       var $cajaAzul = $(".cajaAzul");
+       var tl = new TimelineMax();
+
+       $cajaRoja.animate({
+          width: "+=150",
+          height:  "+=150"
+       }, 500);
+       tl.to( $cajaAzul, 0.5, {
+           width: "+=150",
+           height:  "+=150",
+           backgroundColor: "green"
+       });
+
+   });
    $("button").on("click", function () {
       var dir = $(this).data("dir");
       mover(dir);
