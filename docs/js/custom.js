60b45927284810d28c7b478ac704b319859670f6
smartinez <smartinez@optimeconsulting.com>
2018-12-17 16:45:48 -0500

    Add  // slider show Final Complet

--- docs/js/custom.js
+++ docs/js/custom.js
@@ -1,2 +1,2 @@
-"use strict";
+"use strict";!function(){function n(n,c){c&&clearInterval(s),"sig"===n?i--:i++,i>0?i=-1*(a-1):i<=-1*a&&(i=0);var l=i*t;(new TimelineMax).to(e,1.7,{marginLeft:l,ease:Elastic.easeOut.config(1,.75)})}var i=0,t=1920,e=$(".slideShow ul"),a=e.find("li").length,s=setInterval(function(){n("sig")},1500);$(".js-btn").on("click",function(){n($(this).data("mov"),!0)})}();
 //# sourceMappingURL=custom.js.map
--- src/js/custom.js
+++ src/js/custom.js
@@ -1,4 +1,36 @@
-//
+// funcion Anonima //
 (function () {
    //
+    var actual = 0;
+    var ancho = 1920;
+
+    var $slideshow = $(".slideShow ul");
+    var slides  = $slideshow.find("li").length;
+
+    var intervalo = setInterval(function () {
+       mover("sig");
+    }, 1500);
+
+    function mover( dir, click ) {
+        if ( click )
+            clearInterval( intervalo);
+        ( dir === "sig" ) ? actual -- : actual ++;
+        if ( actual > 0 ){
+            actual = (slides - 1 ) * -1;
+        } else if ( actual  <= (slides * -1)){
+            actual = 0;
+        }
+        var margen = actual * ancho;
+
+        var tl = new TimelineMax();
+
+        tl.to( $slideshow, 1.7, { marginLeft: margen, ease: Elastic.easeOut.config(1, 0.75) } );
+       /* $slideshow.animate({
+            marginLeft: margen
+        }, 450);*/
+    }
+    $(".js-btn").on("click", function () {
+       var dir = $(this).data("mov");
+       mover( dir, true );
+    });
 })();
