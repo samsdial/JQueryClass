8078178e5358e2cae545bea443bb4689087c0e9f
smartinez <smartinez@optimeconsulting.com>
2019-04-10 16:31:35 -0500

    Add  // Card example

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,18 +1,35 @@
-//impertativo vs declarativo
-
-const numeros = [1,2,3,4,5];
-/*CODIGO IMPERATIVO*/
-console.time('Imperativo');
-function squared() {
-    let arr = [];
-    for (let i = 0; i < numeros.length; i++){
-        arr.push(numeros[i] * numeros[i])
+function previewFile() {
+    const preview = document.querySelector('img');
+    const file = document.querySelector('input[type=file]').files[0];
+    const reader = new FileReader();
+    reader.onloadend = function () {
+        preview.src = reader.result;
+    };
+    if (file) {
+        reader.readAsDataURL(file);
+    } else {
+        preview.src = "";
     }
-    return console.log(arr);
 }
-squared(numeros);
-console.timeEnd('Imperativo');
-/*CODDIGO DECLARATIVO*/
-console.time('Declarativo');
-console.log( numeros.map( num => num * num));
-console.timeEnd('Declarativo');
+previewFile();
+
+function genScreenshot() {
+    html2canvas(document.body, {
+        onrendered: function(canvas) {
+            $('#box1').html("");
+            $('#box1').append(canvas);
+
+            if (navigator.userAgent.indexOf("MSIE ") > 0 ||
+                navigator.userAgent.match(/Trident.*rv\:11\./))
+            {
+                var blob = canvas.msToBlob();
+                window.navigator.msSaveBlob(blob,'Test file.png');
+            }
+            else {
+                $('#box1').attr('href', canvas.toDataURL("image/png"));
+                $('#box1').attr('download','Test file.png');
+                $('#box1')[0].click();
+            }
+        }
+    });
+}
